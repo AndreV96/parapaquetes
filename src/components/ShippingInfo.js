3d9596c116c19c89273308764df35c49472a884b
André Vega <ventaslederer@hotmail.com>
2022-04-27 23:58:14 -0500

    Added OrderConfirmation and Shipping Information components.

---
+++ src/components/ShippingInfo.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Container, Form, Button} from 'react-bootstrap';
+
+function ShippingInfo(props) {
+  const billingAddress = props.orderData.billingAddress
+  const shippingAddress = props.orderData.shippingAddress
+  return(
+    <Container className=" text-start">
+      <div className="d-flex justify-content-around pt-1 border-bottom gap-3">
+        <h4 className="f2-bold">Shipping Address</h4>
+        <div className="">
+          <p className="my-0"> {shippingAddress.firstName} {shippingAddress.lastName} </p>
+          <p className="my-0">  {shippingAddress.address1}, {shippingAddress.city}, {shippingAddress.postalCode}, {shippingAddress.country.code} </p>
+          <p className="my-0"> {shippingAddress.phone} </p>
+        </div>  
+      </div>
+      <div className="d-flex justify-content-around pt-1 border-bottom gap-3">
+        <h4 className="f2-bold">Billing Address</h4>
+        <div className="">
+          <p className="my-0"> {billingAddress.firstName} {billingAddress.lastName} </p>
+          <p className="my-0">  {billingAddress.address1}, {billingAddress.city}, {billingAddress.postalCode}, {billingAddress.country.code} </p>
+          <p className="my-0"> {billingAddress.phone} </p>
+        </div>  
+      </div>
+      
+    </Container>
+    
+  )
+}
+
+
+
+
+export default ShippingInfo
