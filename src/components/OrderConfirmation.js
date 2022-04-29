1d69cbddd47bb000f3e591a91d0420dfa92241db
André Vega <ventaslederer@hotmail.com>
2022-04-28 22:10:59 -0500

    Cleaned code, added comment describing funcionality and added README with project description and contact information.

--- src/components/OrderConfirmation.js
+++ src/components/OrderConfirmation.js
@@ -1,59 +1,67 @@
 import React from "react";
-import swal from "sweetalert"
-import {Container, Card, Button} from 'react-bootstrap';
+// Swal framework for confirmation order alert
+import swal from "sweetalert";
+import { Container, Card, Button } from "react-bootstrap";
 
 function OrderConfirmation(props) {
-  const totalCosts = props.orderData.totals
+  // Current order costs information
+  const totalCosts = props.orderData.totals;
 
+  // Order confirmation alert
   const showConfirmationAlert = () => {
-    swal( {
+    swal({
       title: "Thank you for your payment. Your order is on its way!",
       text: `Total: ${props.total}`,
       icon: "success",
       button: "Ok",
-    })
-    props.filterOrder(props.orderData.number)
-  }
-  
-  return(
-    <Container className="d-flex justify-content-center my-3">
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
+    });
+    props.filterOrder(props.orderData.number);
+  };
 
+  return (
+    <Container className="d-flex justify-content-center my-3">
+      {/* Card with all the costs information */}
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
           <Card.Title className="fw-bold">Order Confirmation</Card.Title>
 
-            <div className="d-flex justify-content-between text-green fw-bold">
-              <span>Discount: </span>
-              <span>{totalCosts.discount}</span> 
-            </div>
+          <div className="d-flex justify-content-between text-green fw-bold">
+            <span>Discount: </span>
+            <span>{totalCosts.discount}</span>
+          </div>
 
-            <div className="d-flex justify-content-between">
-              <span>Subtotal: </span>
-              <span>{totalCosts.subtotal}</span> 
-            </div>
+          <div className="d-flex justify-content-between">
+            <span>Subtotal: </span>
+            <span>{totalCosts.subtotal}</span>
+          </div>
 
-            <div className="d-flex justify-content-between">
-              <span>Shipping: </span>
-              <span>{totalCosts.shipping}</span> 
-            </div>
+          <div className="d-flex justify-content-between">
+            <span>Shipping: </span>
+            <span>{totalCosts.shipping}</span>
+          </div>
 
-            <div className="d-flex justify-content-between border-bottom ">
-              <span>Tax: </span>
-              <span>{totalCosts.tax}</span> 
-            </div>
+          <div className="d-flex justify-content-between border-bottom ">
+            <span>Tax: </span>
+            <span>{totalCosts.tax}</span>
+          </div>
 
-            <div className="d-flex justify-content-between fw-bold mb-3 fs-5 text-red">
-              <span>Total: </span>
-              <span>{props.total}</span> 
-            </div>
+          <div className="d-flex justify-content-between fw-bold mb-3 fs-5 text-red">
+            <span>Total: </span>
+            <span>{props.total}</span>
+          </div>
 
-            <Button onClick={showConfirmationAlert} variant="outline-primary" className="add-confirm-button">Order Now & Pay</Button>
+          {/* Submit order button */}
+          <Button
+            onClick={showConfirmationAlert}
+            variant="outline-primary"
+            className="add-confirm-button"
+          >
+            Order Now & Pay
+          </Button>
         </Card.Body>
       </Card>
     </Container>
-  )
+  );
 }
 
-
-
-export default OrderConfirmation
+export default OrderConfirmation;
